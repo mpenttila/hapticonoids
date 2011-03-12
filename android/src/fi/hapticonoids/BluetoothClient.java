219701e1fdb5c0b9f4c0e8078d2251574a6f74a1
vpk <vpk@10.0.0.103>
2011-03-12 10:45:17 +0200

    More BT related changes. Now need the Server side server to continue testing.

--- android/src/fi/hapticonoids/BluetoothClient.java
+++ android/src/fi/hapticonoids/BluetoothClient.java
@@ -1,16 +1,89 @@
 package fi.hapticonoids;
 
-import android.os.Handler;
+import java.util.Set;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothClass.Device;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.app.Activity;
+import android.os.Looper;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 public class BluetoothClient extends Thread {
 
+	private static final int REQUEST_ENABLE_BT = 3;
+	BluetoothAdapter mBluetoothAdapter;
 	MessageEater[] eaterarray;
+	Activity activity;
+	ArrayAdapter<String> btDeviceArray;
 	
-	public BluetoothClient(MessageEater[] eaterarray) {
+	public BluetoothClient(MessageEater[] eaterarray, Activity activity) {
+		this.activity = activity;
 		this.eaterarray = eaterarray;
+        this.mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        // If the adapter is null, then Bluetooth is not supported
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this.activity, "Bluetooth is not available", Toast.LENGTH_LONG).show();
+            this.activity.finish();
+            return;
+        }
+        //this.enableBT();
+
+        
 	}
 	
 	public void run() {
+		Looper.prepare();
+        // If BT is not on, request that it be enabled.
+        // setupChat() will then be called during onActivityResult
+        Toast.makeText(this.activity, "Trying to enable BT", Toast.LENGTH_LONG).show();
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            this.activity.startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+            Toast.makeText(this.activity, "Enabled BT", Toast.LENGTH_LONG).show();
+        }
+        else
+        	Toast.makeText(this.activity, "BT Allready on!", Toast.LENGTH_LONG).show();
+        
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        // If there are paired devices
+        if (pairedDevices.size() > 0) {
+        	// Loop through paired devices
+        	for (BluetoothDevice device : pairedDevices) {
+        		// Add the name and address to an array adapter to show in a ListView
+        		this.btDeviceArray.add(device.getName() + "\n" + device.getAddress());
+        		//Toast.makeText(this.activity, device.getName() + "\n" + device.getAddress(), Toast.LENGTH_LONG).show();
+        	}
+        }
+        
+        // Create a BroadcastReceiver for ACTION_FOUND
+        System.out.println("Recive devices");
+        final BroadcastReceiver mReceiver = new BroadcastReceiver() {        	
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                // When discovery finds a device
+                if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                    // Get the BluetoothDevice object from the Intent
+                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                    // Add the name and address to an array adapter to show in a ListView
+                    //this.btDeviceArray.add(device.getName() + "\n" + device.getAddress());
+                    System.out.println(device.getName() + "\n" + device.getAddress());
+                }
+            }
+        };
+        // Register the BroadcastReceiver
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        this.activity.registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy        
+        
+        Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+        this.activity.startActivity(discoverableIntent);        
+        
 		for (int i = 0; i < 2; i++) {
 			try {
 				sleep(5000);
@@ -21,6 +94,7 @@ public class BluetoothClient extends Thread {
 			eaterarray[0].doTask(1);
 		}
 		eaterarray[0].stopListener();
+		Looper.loop();		
 	}
 	
 }
--- android/src/fi/hapticonoids/Hapticonoids.java
+++ android/src/fi/hapticonoids/Hapticonoids.java
@@ -1,22 +1,14 @@
 package fi.hapticonoids;
 
-import java.util.Set;
 import android.app.Activity;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
 import android.os.Vibrator;
-import android.widget.Toast;
 
 public class Hapticonoids extends Activity {
 	
-	private static final int REQUEST_ENABLE_BT = 3;
+	
 	Vibrator vibra; 
-	BluetoothAdapter mBluetoothAdapter;	
 	
     /** Called when the activity is first created. */
     @Override
@@ -24,36 +16,19 @@ public class Hapticonoids extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        this.mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        // If the adapter is null, then Bluetooth is not supported
-        if (mBluetoothAdapter == null) {
-            Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG).show();
-            finish();
-            return;
-        }
 
-        //this.enableBT();
         
         this.vibra = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);        
     	VibratorThread vt = new VibratorThread(this.vibra);
     	vt.start();    	
     	MessageEater[] eaterarray = {vt};
-    	BluetoothClient bs = new BluetoothClient(eaterarray);
+    	BluetoothClient bs = new BluetoothClient(eaterarray, this);
     	bs.start();
     }
     
     @Override
     public void onStart() {
-        super.onStart();        
-        // If BT is not on, request that it be enabled.
-        // setupChat() will then be called during onActivityResult
-        Toast.makeText(this, "Trying to enable BT", Toast.LENGTH_LONG).show();
-        if (!mBluetoothAdapter.isEnabled()) {
-            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
-            Toast.makeText(this, "Enabled BT", Toast.LENGTH_LONG).show();
-        }
-        Toast.makeText(this, "BT Allready on!", Toast.LENGTH_LONG).show();
+        super.onStart();
     }
 
 }

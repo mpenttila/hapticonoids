66a8da80374dc760f771a5481a4553fcf54763ea
vpk <vpk@10.0.0.103>
2011-03-15 22:23:05 +0200

    Android changes for getting BT work. Problem with service discovery and initial connection.

--- android/src/fi/hapticonoids/BluetoothClient.java
+++ android/src/fi/hapticonoids/BluetoothClient.java
@@ -1,10 +1,15 @@
 package fi.hapticonoids;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Set;
+import java.util.UUID;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothClass.Device;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -17,10 +22,13 @@ import android.widget.Toast;
 public class BluetoothClient extends Thread {
 
 	private static final int REQUEST_ENABLE_BT = 3;
-	BluetoothAdapter mBluetoothAdapter;
-	MessageEater[] eaterarray;
-	Activity activity;
-	ArrayAdapter<String> btDeviceArray;
+	private BluetoothAdapter mBluetoothAdapter;
+	private MessageEater[] eaterarray;
+	private Activity activity;
+	private BluetoothSocket game = null;
+	private InputStream btIn = null;
+	private OutputStream btOut = null;	
+	
 	
 	public BluetoothClient(MessageEater[] eaterarray, Activity activity) {
 		this.activity = activity;
@@ -32,67 +40,63 @@ public class BluetoothClient extends Thread {
             this.activity.finish();
             return;
         }
-        //this.enableBT();
-
+        // Enable BT
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            this.activity.startActivityForResult(enableIntent, REQUEST_ENABLE_BT);    
+        }
+        
+        // Get paired devices (We don't currently pair automatically)        
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();        	
+        if (pairedDevices.size() > 0) {
+            for (BluetoothDevice device : pairedDevices) {
+            	if (device.getName().equals("game")) {
+            		try {
+            			//UUID tmp = UUID.fromString("1998ea20-cfca-4619-837d-b36b04fde3d5");            			
+            			UUID tmp = UUID.fromString("20ea9819-1946-cacf-6bb3-7d83d5e3fd04");
+            			this.game = device.createRfcommSocketToServiceRecord(tmp);
+            			this.game.connect();
+                        this.btIn = this.game.getInputStream();
+                        this.btOut = this.game.getOutputStream();            			
+            		} catch (IOException e) {
+            			System.out.println("Connection failed!" + e.getMessage());
+            			if (this.game != null) {
+	            			try {
+								this.game.close();
+							} catch (IOException e1) {
+								// TODO Auto-generated catch block
+								e1.printStackTrace();
+							}
+	            			this.game = null;
+            			}
+            		}
+            	}
+            }
+        }
         
 	}
 	
 	public void run() {
 		Looper.prepare();
-        // If BT is not on, request that it be enabled.
-        // setupChat() will then be called during onActivityResult
-        Toast.makeText(this.activity, "Trying to enable BT", Toast.LENGTH_LONG).show();
-        if (!mBluetoothAdapter.isEnabled()) {
-            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            this.activity.startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
-            Toast.makeText(this.activity, "Enabled BT", Toast.LENGTH_LONG).show();
-        }
-        else
-        	Toast.makeText(this.activity, "BT Allready on!", Toast.LENGTH_LONG).show();
-        
-        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-        // If there are paired devices
-        if (pairedDevices.size() > 0) {
-        	// Loop through paired devices
-        	for (BluetoothDevice device : pairedDevices) {
-        		// Add the name and address to an array adapter to show in a ListView
-        		this.btDeviceArray.add(device.getName() + "\n" + device.getAddress());
-        		//Toast.makeText(this.activity, device.getName() + "\n" + device.getAddress(), Toast.LENGTH_LONG).show();
-        	}
-        }
-        
-        // Create a BroadcastReceiver for ACTION_FOUND
-        System.out.println("Recive devices");
-        final BroadcastReceiver mReceiver = new BroadcastReceiver() {        	
-            public void onReceive(Context context, Intent intent) {
-                String action = intent.getAction();
-                // When discovery finds a device
-                if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                    // Get the BluetoothDevice object from the Intent
-                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                    // Add the name and address to an array adapter to show in a ListView
-                    //this.btDeviceArray.add(device.getName() + "\n" + device.getAddress());
-                    System.out.println(device.getName() + "\n" + device.getAddress());
-                }
-            }
-        };
-        // Register the BroadcastReceiver
-        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        this.activity.registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy        
-        
-        Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
-        this.activity.startActivity(discoverableIntent);        
-        
-		for (int i = 0; i < 2; i++) {
-			try {
-				sleep(5000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				System.out.println("Exception while sleeping.");
-			}
-			eaterarray[0].doTask(1);
+		byte[] buffer = new byte[1024];
+		int bytes;
+		System.out.println("Running the reciver loop!");
+		if (this.game == null) {
+			System.out.println("Connection failed. Sorry. Game not connected :(");
+			this.activity.finish();
+			return;
 		}
+		while (true) {	
+			try {
+				bytes = this.btIn.read(buffer);
+				System.out.println(new String(buffer));
+				eaterarray[0].doTask(1);
+			} catch (IOException e) {
+				System.out.println("Reciver loop ended!" + e.getMessage());
+				this.activity.finish();
+				break; 
+			}
+		}		
 		eaterarray[0].stopListener();
 		Looper.loop();		
 	}

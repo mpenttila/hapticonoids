9a4d51f352515a33e0a664922db9e154d7218033
vpk <github@guagua.fi>
2011-04-08 22:33:27 +0300

    Sound functionality for testing.

---
+++ android/src/fi/hapticonoids/SoundThread.java
@@ -0,0 +1,68 @@
+package fi.hapticonoids;
+
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Handler;
+import android.os.Looper;
+
+public class SoundThread extends Thread implements MessageEater {
+
+	Handler msgQueue = null;
+	private static int type = 1;
+	Hapticonoids hapticonoids = null;
+	SoundPool sounds = null;
+	int[] soundsIndex = new int[5];
+
+	public SoundThread(Hapticonoids hapticonoids) {
+		this.hapticonoids = hapticonoids;
+		this.sounds = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+		soundsIndex[0] = this.sounds.load(hapticonoids, R.raw.thud, 1);
+	}
+
+	@Override
+	public void run() {
+		System.out.println("Starting prepare");
+		Looper.prepare();
+		System.out.println("Creating handler");
+		this.msgQueue = new Handler();
+		System.out.println("Starting loop");
+        Looper.loop();
+	}	
+	
+	public int getType() {
+		return SoundThread.type;
+	}
+
+	public synchronized void stopListener() {
+		this.msgQueue.post(new Runnable() {
+			public void run() {
+				Looper.myLooper().quit();				
+			}
+		});
+	}
+
+	private class SoundRunnable implements Runnable {
+		int id = -1;
+		SoundThread st = null;
+		
+		public SoundRunnable(SoundThread st, int id) {
+			this.id = id;
+			this.st = st;
+		}
+		
+		public void run() {
+			switch(this.id) {
+			case 1:
+				this.st.sounds.play(this.st.soundsIndex[0], (float)1.0, (float)1.0, 0, 0, (float)1.0);
+				System.out.println("Thud!");
+				break;
+			}
+		}		
+	}
+	
+	public synchronized void doTask(int id) {
+		SoundRunnable sr = new SoundRunnable(this, id);		
+		this.msgQueue.post(sr);
+	}
+
+}

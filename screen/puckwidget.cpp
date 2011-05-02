b29f29e4f15f6e5aacb63212516c5efb688b72b5
Markus Penttilä <wox@iki.fi>
2011-05-02 20:56:02 +0300

    Some cleanup.

--- screen/puckwidget.cpp
+++ screen/puckwidget.cpp
@@ -5,7 +5,6 @@
 #include <iostream>
 
 PuckWidget::PuckWidget(MultiWidgets::Widget * parent) : MultiWidgets::ImageWidget(parent) {
-  //setInputFlags(inputFlags() & ~INPUT_PASS_TO_CHILDREN);
 }
 
 void PuckWidget::input(MultiWidgets::GrabManager & gm, float dt)
